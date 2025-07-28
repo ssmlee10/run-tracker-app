ae4f280d41328c18c1105b7c1a0df87327007dad
Sharon Lee <ssmlee10@gmail.com>
2025-07-28 15:01:33 -0700

    create runSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,35 @@
 const mongoose = require('mongoose');
 
+// define run schema
+const runSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
+    distance: {
+        type: Number,
+        required: true,
+    },
+    distanceUnit: {
+        type: String,
+        enum: ["km", "mi"],
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    // pace will be calculated on show page
+    caloriesBurnt: Number,
+    averageHr: Number,
+    runType: {
+        type: String,
+        enum: ['easy', 'long', 'tempo', 'race', 'interval'],
+        required: true,
+    },
+    notes: String,
+});
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
