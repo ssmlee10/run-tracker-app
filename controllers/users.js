10fbb8aa55fd3b003dcf1bc0beff9bb891a26486
Sharon Lee <ssmlee10@gmail.com>
2025-07-31 14:21:45 -0700

    create community function

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,21 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// GET to the users index page
+router.get('/', async (req, res) => {
+    const allUsers = await User.find({});
+    res.render('users/index.ejs', {users: allUsers, });
+});
+
 // GET to individual users' runs
 router.get('/:id', async (req, res) => {
-    res.render('users/index.ejs');
-})
+    try {
+        const user = await User.findById(req.params.id).populate('runs');
+        res.render('users/show.ejs', { user });
+    } catch(error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
